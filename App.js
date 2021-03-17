9708b1ef31c51c214959ed3869132a018591a84c
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2021-03-17 23:31:46 +0000

    transfered some files over from the old local repositry

---
+++ App.js
@@ -0,0 +1,48 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
+import ProfileScreen from './views/profile';
+import HomeScreen from './views/home';
+
+export default function App() {
+  return (
+    <MyStack />
+    // <NavigationContainer>
+    //   <View style={styles.container}>
+    //     <Text>Open up App.js to start working on your app!</Text>
+    //     <StatusBar style="auto" />
+    //   </View>
+    // </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
